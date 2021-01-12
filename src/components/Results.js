9b307e1f6710f63ffe0fc1bcc3e98be17d92ad37
khizerdev <46632721+khizerdev@users.noreply.github.com>
2021-01-12 16:04:15 +0500

    API Integration

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import './Results.css'
 import VideoCard from './VideoCard'
+import {useState ,useEffect} from 'react'
+import axios from '../axios'
+import request from '../request'
 
 function Results() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const requestURL  = await axios.get(request.fetchActionMovies)
+            console.log(requestURL)
+        }
+
+        fetchData()
+    }, []);
+
     return (
         <div className="results">
+        {
+            movies.map((movie) => (
             <VideoCard/>
+            ))
+        }
         </div>
     )
 }
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './VideoCard.css'
 
 function VideoCard() {
     return (
-        <div>
-            
+        <div className="videoCard">
+            <img src="https://iqonic.design/themes/streamitnew/frontend/html/images/episodes/01.jpg" alt=""/>
+            <p>Coding Movie</p>
+            <h2>Movie Title</h2>
+            <p>No of Likes 767</p>
         </div>
     )
 }
